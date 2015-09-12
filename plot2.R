09af62a6c0671442310200dc0d64800979112e60
Esa Jaakola <epen.tunnus@gmail.com>
2015-09-12 14:29:17 +0300

    Script and pic for second plot

---
+++ plot2.R
@@ -0,0 +1,35 @@
+## plot2 function
+##
+## Reads household consumption data from given URL, selects certain dates
+## and plots active power consumption per selected days as line into PNG file
+
+plot2 <- function() {
+    ## Download data package
+    if(!file.exists("./data")) {
+        dir.create("./data")
+    }
+    fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+    download.file(fileUrl, destfile="./data/power.zip", method="curl")
+    unzip("./data/power.zip", exdir="./data")
+    
+    ## Read data
+    data <- read.table("./data/household_power_consumption.txt", header = TRUE, sep=";", as.is = TRUE, na.strings="?")
+    data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
+
+    ## Select only dates 2007-02-01 and 2007-02-02
+    newdata <- subset(data, Date == "2007-02-01" | Date == "2007-02-02", 
+                      select=c(Global_active_power, Date, Time))
+    
+    ## Add new datetime field for sorting
+    newdata$Datetime <- as.POSIXct(paste(newdata$Date, newdata$Time), format="%Y-%m-%d %H:%M:%S")
+    ## Sort per datetime
+    attach(newdata)
+    newdata <- newdata[order(Datetime),]
+    detach(newdata)
+    
+    ## Create plot & write to file
+    png("plot2.png")
+    with(newdata, plot(Datetime, Global_active_power, main="",
+         ylab="Global Active Power (kilowatts)", xlab="", type="l"))
+    dev.off()
+}
